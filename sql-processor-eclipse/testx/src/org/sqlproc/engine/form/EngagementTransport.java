b7a73b6396e0a7c62fcffb87c85c5196ee8aa989
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2011-06-15 09:34:04 +0200

    Issue #21: Testing example

---
+++ sql-processor-eclipse/testx/src/org/sqlproc/engine/form/EngagementTransport.java
@@ -0,0 +1,35 @@
+package org.sqlproc.engine.form;
+
+public class EngagementTransport {
+	private String first;
+	private String last;
+	private String role;
+	private String ssn;
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+	
+}
---
+++ sql-processor-eclipse/testx/src/org/sqlproc/engine/form/NameCollectionsForm.java
@@ -0,0 +1,24 @@
+package org.sqlproc.engine.form;
+
+import java.util.List;
+
+public class NameCollectionsForm {
+    private List<String> firstList;
+    private List<String> lastList;
+
+    public List<String> getFirstList() {
+        return firstList;
+    }
+
+    public void setFirstList(List<String> firstList) {
+        this.firstList = firstList;
+    }
+
+    public List<String> getLastList() {
+        return lastList;
+    }
+
+    public void setLastList(List<String> lastList) {
+        this.lastList = lastList;
+    }
+}
