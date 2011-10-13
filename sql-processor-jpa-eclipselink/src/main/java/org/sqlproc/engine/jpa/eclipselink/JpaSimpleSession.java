a4eb82029f2c4bf1b6c86669f211de93b58542fd
Marek Winkler <winkler.marek@gmail.com>
2011-10-13 16:08:17 +0200

    Issue #38: Updated javadoc

--- sql-processor-jpa-eclipselink/src/main/java/org/sqlproc/engine/jpa/eclipselink/JpaSimpleSession.java
+++ sql-processor-jpa-eclipselink/src/main/java/org/sqlproc/engine/jpa/eclipselink/JpaSimpleSession.java
@@ -9,40 +9,49 @@ import org.sqlproc.engine.SqlQueryEngine;
 import org.sqlproc.engine.SqlSession;
 
 /**
- * The JPA stack implementation of the SQL Engine session contract. In fact it's a simple wrapper for the
- * {@link org.hibernate.Session}.
- * 
- * It's the first parameter to all primary methods in the {@link SqlQueryEngine} and {@link SqlCrudEngine}.
- * 
- * <p>
+ * The JPA 2 with EclipseLink stack implementation of the SQL Engine session contract. In fact it's a simple wrapper for
+ * the JDBC connection obtained from the {@link javax.persistence.EntityManager}.
+ * <p/>
  * The primary contract is the method for the {@link SqlQuery} instance creation.
- * 
- * <p>
+ * <p/>
+ * It's the first parameter to all primary methods in the {@link SqlQueryEngine} and {@link SqlCrudEngine}.
+ * <p/>
+ * The EntityManager is used only to enable convenient JPA-style configuration, the SQL Engine uses only the
+ * {@link java.sql.Connection} provided by EntityManager.
+ * <p/>
+ * It is strongly recommended that the EntityManager instance used to create the SqlSession instance should be used
+ * exclusively by the SQL Processor, i.e. it should NOT be used for querying/persisting entities, construction of JPQL
+ * or native SQL queries, etc.
+ * <p/>
+ * Also note, that when the EntityManager is bound to a transaction, so are the SQL Processor operations using the
+ * {@link SqlSession} constructed from this EntityManager.
+ * <p/>
  * For more info please see the <a href="https://github.com/hudec/sql-processor/wiki">Tutorials</a>.
  * 
- * @author <a href="mailto:Vladimir.Hudec@gmail.com">Vladimir Hudec</a>
+ * @author <a href="mailto:winkler.marek@gmail.com">Marek Winkler</a>
+ * @author <a href="mailto:ondrej.kotek@gmail.com">Ondrej Kotek</a>
  */
 public class JpaSimpleSession implements SqlSession {
 
     /**
-     * The Hibernate Session, the first level cache and the context for all database operations.
+     * The EntityManager, the first level cache and the context for all database operations.
      */
     private final EntityManager entityManager;
 
     /**
-     * Creates a new instance of this dynamic proxy.
+     * Creates a new instance of this session.
      * 
      * @param entityManager
-     *            the Hibernate Session instance
+     *            the EntityManager instance which will provide the database connection, must not be null
      */
     public JpaSimpleSession(final EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
     /**
-     * Returns the internal Hibernate Session instance.
+     * Returns the internal EntityManager instance whose database connection is used by SQL Processor.
      * 
-     * @return the internal Hibernate Session instance
+     * @return the EntityManager instance
      */
     public EntityManager getEntityManager() {
         return this.entityManager;
--- sql-processor-jpa-eclipselink/src/main/java/org/sqlproc/engine/jpa/eclipselink/package-info.java
+++ sql-processor-jpa-eclipselink/src/main/java/org/sqlproc/engine/jpa/eclipselink/package-info.java
@@ -1,25 +1,41 @@
 /**
- * The adapters and proxies for the JPA stack. TODO documentation
+ * The adapters and proxies for the JPA 2 over EclipseLink stack obtaining the database access from
+ * the {@link javax.persistence.EntityManager}.
  * 
  * The simple usage can be the next one:
  * 
  * <pre>
- * HibernateEngineFactory sqlFactory = new HibernateEngineFactory();
+ * JpaEngineFactory sqlFactory = new JpaEngineFactory();
  * sqlFactory.setMetaFilesNames(&quot;statements.qry&quot;); // the meta statements file
  * SqlQueryEngine sqlEngine = sqlFactory.getQueryEngine(&quot;ALL_PEOPLE&quot;);
  * 
- * // for the case it runs on the top of the Hibernate stack
- * Configuration configuration = new Configuration().configure(&quot;hibernate.cfg.xml&quot;);
- * SessionFactory sessionFactory = configuration.buildSessionFactory();
- * SqlSession session = HibernateSession.generateProxy(sessionFactory.openSession());
+ * // for the case it runs on the top of the EclipseLink stack in Java EE managed environment, just use the injected EntityManager
+ * SqlSession session = new JpaSimpleSession(entityManager);
  * 
  * List&lt;Person&gt; list = sqlEngine.query(session, Person.class, null, SqlQueryEngine.ASC_ORDER);
- * </pre>
+ *
+ * // in the case of non-managed environment, you can construct the EntityManager as follows:  
+ * EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+ * EntityManager entityManager = entityManagerFactory.createEntityManager();
+ * JpaSimpleSession session = new JpaSimpleSession(entityManager);
  * 
- * <p>
+ * List&lt;Person&gt; list = sqlEngine.query(session, Person.class, null, SqlQueryEngine.ASC_ORDER);
+ *
+ * entityManager.close();
+ * entityManagerFactory.close();
+ * 
+ * </pre>
+ * It is strongly recommended that the EntityManager instance used to create the SqlSession instance should be used
+ * exclusively by the SQL Processor, i.e. it should NOT be used for querying/persisting entities,
+ * construction of JPQL or native SQL queries, etc.
+ * <p/>
+ * Also note, that when the EntityManager is bound to a transaction, so are the SQL Processor operations using
+ * the {@link org.sqlproc.engine.SqlSession} constructed from this EntityManager. 
+ * <p/>
  * For more info please see the <a href="https://github.com/hudec/sql-processor/wiki">Tutorials</a>.
  * 
- * @author <a href="mailto:Vladimir.Hudec@gmail.com">Vladimir Hudec</a>
+ * @author <a href="mailto:winkler.marek@gmail.com">Marek Winkler</a>
+ * @author <a href="mailto:ondrej.kotek@gmail.com">Ondrej Kotek</a>
  */
 package org.sqlproc.engine.jpa.eclipselink;
 
