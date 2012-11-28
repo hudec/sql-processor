d014a1c4894753b3e4ee5b2b72f99cbe58ef4bf7
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2012-11-28 10:47:38 +0100

    Issue #73: Fixed extended filters highlighting

--- sql-processor-eclipse/org.sqlproc.dsl.ui/src/org/sqlproc/dsl/ui/syntaxcoloring/SemanticHighlightingCalculator.java
+++ sql-processor-eclipse/org.sqlproc.dsl.ui/src/org/sqlproc/dsl/ui/syntaxcoloring/SemanticHighlightingCalculator.java
@@ -209,15 +209,11 @@ public class SemanticHighlightingCalculator implements ISemanticHighlightingCalc
     private void provideHighlightingForFilters(EList<String> filters, ICompositeNode node,
             IHighlightedPositionAcceptor acceptor) {
         if (filters != null && !filters.isEmpty()) {
-            int count = filters.size();
             Iterator<INode> iterator = new NodeTreeIterator(node);
             while (iterator.hasNext()) {
                 INode inode = iterator.next();
-                // System.out.println("XXXXX " + inode.getText() + " " + inode);
                 if (filters.contains(inode.getText())) {
                     acceptor.addPosition(inode.getOffset(), inode.getLength(), HighlightingConfiguration.FILTER);
-                    if (--count <= 0)
-                        return;
                 }
             }
         }
