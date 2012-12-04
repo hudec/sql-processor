56f909cc28f6ad216c4f5340053fa07a5b0891cd
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2012-12-04 10:47:29 +0100

    Issue #83 Meta SQL generator

--- sql-samples/simple-jdbc-crud/src/main/java/org/sqlproc/sample/simple/Main.java
+++ sql-samples/simple-jdbc-crud/src/main/java/org/sqlproc/sample/simple/Main.java
@@ -14,8 +14,16 @@ import org.sqlproc.engine.SqlSession;
 import org.sqlproc.engine.jdbc.JdbcEngineFactory;
 import org.sqlproc.engine.jdbc.JdbcSimpleSession;
 import org.sqlproc.engine.util.DDLLoader;
+import org.sqlproc.sample.simple.model.BankAccount;
+import org.sqlproc.sample.simple.model.Book;
 import org.sqlproc.sample.simple.model.Contact;
+import org.sqlproc.sample.simple.model.CreditCard;
+import org.sqlproc.sample.simple.model.Library;
+import org.sqlproc.sample.simple.model.Media;
+import org.sqlproc.sample.simple.model.Movie;
 import org.sqlproc.sample.simple.model.Person;
+import org.sqlproc.sample.simple.model.PersonLibrary;
+import org.sqlproc.sample.simple.model.Subscriber;
 import org.sqlproc.sample.simple.type.PhoneNumberType;
 
 public class Main {
@@ -71,22 +79,94 @@ public class Main {
     public Person insertPerson(Person person) {
         SqlCrudEngine sqlInsertPerson = sqlFactory.getCrudEngine("INSERT_PERSON");
         int count = sqlInsertPerson.insert(session, person);
-        logger.info("insert: " + count + ": " + person);
+        logger.info("insert person: " + count + ": " + person);
         return (count > 0) ? person : null;
     }
 
-    public Person insertContacts(Person person, Contact... contacts) {
+    public Person insertPersonContacts(Person person, Contact... contacts) {
         SqlCrudEngine sqlInsertContact = sqlFactory.getCrudEngine("INSERT_CONTACT");
         if (contacts != null) {
             for (Contact contact : contacts) {
                 contact.setPerson(person);
                 int count = sqlInsertContact.insert(session, contact);
-                logger.info("insert: " + count + ": " + contact);
+                logger.info("insert contact: " + count + ": " + contact);
                 if (count > 0)
                     person.getContacts().add(contact);
             }
         }
         return person;
+    }
+
+    public Library insertLibrary(Library library) {
+        SqlCrudEngine sqlInsertLibrary = sqlFactory.getCrudEngine("INSERT_LIBRARY");
+        int count = sqlInsertLibrary.insert(session, library);
+        logger.info("insert library: " + count + ": " + library);
+        return (count > 0) ? library : null;
+    }
+
+    public Subscriber insertLibrarySubscriber(Library library, Subscriber subscriber) {
+        SqlCrudEngine sqlInsertSubscriber = sqlFactory.getCrudEngine("INSERT_SUBSCRIBER");
+        if (subscriber != null) {
+            subscriber.setLibrary(library);
+            int count = sqlInsertSubscriber.insert(session, subscriber);
+            logger.info("insert subscriber: " + count + ": " + subscriber);
+            if (count > 0)
+                library.getSubscribers().add(subscriber);
+        }
+        return subscriber;
+    }
+
+    public BankAccount insertBankAccount(BankAccount bankAccount) {
+        SqlCrudEngine sqlInsertBankAccount = sqlFactory.getCrudEngine("INSERT_BANK_ACCOUNT");
+        int count = sqlInsertBankAccount.insert(session, bankAccount);
+        logger.info("insert bank account: " + count + ": " + bankAccount);
+        if (count > 0)
+            bankAccount.getSubscriber().getBillingDetails().add(bankAccount);
+        return (count > 0) ? bankAccount : null;
+    }
+
+    public CreditCard insertCreditCard(CreditCard creditCard) {
+        SqlCrudEngine sqlInsertCreditCard = sqlFactory.getCrudEngine("INSERT_CREDIT_CARD");
+        int count = sqlInsertCreditCard.insert(session, creditCard);
+        logger.info("insert credit card: " + count + ": " + creditCard);
+        if (count > 0)
+            creditCard.getSubscriber().getBillingDetails().add(creditCard);
+        return (count > 0) ? creditCard : null;
+    }
+
+    public Movie insertMovie(Movie movie) {
+        SqlCrudEngine sqlInsertMedia = sqlFactory.getCrudEngine("INSERT_MEDIA");
+        SqlCrudEngine sqlInsertMovie = sqlFactory.getCrudEngine("INSERT_MOVIE");
+        int count = sqlInsertMedia.insert(session, movie);
+        if (count > 0) {
+            sqlInsertMovie.insert(session, movie);
+        }
+        logger.info("insert movie: " + count + ": " + movie);
+        return (count > 0) ? movie : null;
+    }
+
+    public Book insertBook(Book book) {
+        SqlCrudEngine sqlInsertMedia = sqlFactory.getCrudEngine("INSERT_MEDIA");
+        SqlCrudEngine sqlInsertBook = sqlFactory.getCrudEngine("INSERT_BOOK");
+        int count = sqlInsertMedia.insert(session, book);
+        if (count > 0) {
+            sqlInsertBook.insert(session, book);
+        }
+        logger.info("insert book: " + count + ": " + book);
+        return (count > 0) ? book : null;
+    }
+
+    public void insertPersonLibrary(Person person, Media... media) {
+        SqlCrudEngine sqlInsertPersonLibrary = sqlFactory.getCrudEngine("INSERT_PERSON_LIBRARY");
+        if (media != null) {
+            for (Media media1 : media) {
+                PersonLibrary personLibrary = new PersonLibrary(person.getId(), media1.getId());
+                int count = sqlInsertPersonLibrary.insert(session, personLibrary);
+                logger.info("insert person library: " + count + ": " + personLibrary);
+                if (count > 0)
+                    person.getLibrary().add(media1);
+            }
+        }
     }
 
     //
@@ -151,36 +231,6 @@ public class Main {
     // return list;
     // }
     //
-    // public Movie insertMovie(Movie movie) {
-    // SqlCrudEngine sqlInsertMedia = sqlFactory.getCrudEngine("INSERT_MEDIA");
-    // SqlCrudEngine sqlInsertMovie = sqlFactory.getCrudEngine("INSERT_MOVIE");
-    // int count = sqlInsertMedia.insert(session, movie);
-    // if (count > 0) {
-    // sqlInsertMovie.insert(session, movie);
-    // }
-    // return (count > 0) ? movie : null;
-    // }
-    //
-    // public Book insertBook(Book book) {
-    // SqlCrudEngine sqlInsertMedia = sqlFactory.getCrudEngine("INSERT_MEDIA");
-    // SqlCrudEngine sqlInsertBook = sqlFactory.getCrudEngine("INSERT_BOOK");
-    // int count = sqlInsertMedia.insert(session, book);
-    // if (count > 0) {
-    // sqlInsertBook.insert(session, book);
-    // }
-    // return (count > 0) ? book : null;
-    // }
-    //
-    // public void createPersonLibrary(Person person, Media... media) {
-    // SqlCrudEngine sqlCreateLibrary = sqlFactory.getCrudEngine("CREATE_LIBRARY");
-    // if (media == null || media.length == 0)
-    // return;
-    // for (Media media1 : media) {
-    // PersonLibrary library = new PersonLibrary(person.getId(), media1.getId());
-    // sqlCreateLibrary.insert(session, library);
-    // }
-    // }
-    //
     // public List<Person> listPeopleLibrary(Person person) {
     // SqlQueryEngine sqlEngine = sqlFactory.getQueryEngine("ALL_PEOPLE_LIBRARY");
     // Map<String, Class<?>> moreResultClasses = new HashMap<String, Class<?>>();
@@ -192,50 +242,11 @@ public class Main {
     // return list;
     // }
     //
-    // public Person insertCustom(Person person, Contact... contacts) {
-    // SqlCrudEngine sqlInsertPerson = sqlFactory.getCrudEngine("INSERT_PERSON");
-    // SqlCrudEngine sqlInsertContact = sqlFactory.getCrudEngine("INSERT_CONTACT_CUSTOM");
-    // int count = sqlInsertPerson.insert(session, person);
-    // logger.info("insert: " + count);
-    // logger.info("insert: " + person);
-    // if (contacts != null && count > 0) {
-    // for (Contact contact : contacts) {
-    // contact.setPerson(person);
-    // sqlInsertContact.insert(session, contact);
-    // }
-    // }
-    // return (count > 0) ? person : null;
-    // }
-    //
     // public List<Person> listCustom(Contact contact) {
     // SqlQueryEngine sqlEngine = sqlFactory.getQueryEngine("ALL_PEOPLE_AND_CONTACTS_CUSTOM");
     // List<Person> list = sqlEngine.query(session, Person.class, contact, SqlQueryEngine.ASC_ORDER);
     // logger.info("listCustom size: " + list.size());
     // return list;
-    // }
-    //
-    // public Library insertLibrary(Library library) {
-    // SqlCrudEngine sqlInsertLibrary = sqlFactory.getCrudEngine("INSERT_LIBRARY");
-    // int count = sqlInsertLibrary.insert(session, library);
-    // return (count > 0) ? library : null;
-    // }
-    //
-    // public Subscriber insertSubscriber(Subscriber subscriber) {
-    // SqlCrudEngine sqlInsertSubscriber = sqlFactory.getCrudEngine("INSERT_SUBSCRIBER");
-    // int count = sqlInsertSubscriber.insert(session, subscriber);
-    // return (count > 0) ? subscriber : null;
-    // }
-    //
-    // public BankAccount insertBankAccount(BankAccount bankAccount) {
-    // SqlCrudEngine sqlInsertBankAccount = sqlFactory.getCrudEngine("INSERT_BANK_ACCOUNT");
-    // int count = sqlInsertBankAccount.insert(session, bankAccount);
-    // return (count > 0) ? bankAccount : null;
-    // }
-    //
-    // public CreditCard insertCreditCard(CreditCard creditCard) {
-    // SqlCrudEngine sqlInsertCreditCard = sqlFactory.getCrudEngine("INSERT_CREDIT_CARD");
-    // int count = sqlInsertCreditCard.insert(session, creditCard);
-    // return (count > 0) ? creditCard : null;
     // }
     //
     // public List<Subscriber> listAllSubsribersWithBillingDetails() {
@@ -283,36 +294,38 @@ public class Main {
         Main main = new Main();
         main.setupDb();
 
-        // init
+        // insert
         Person jan = main.insertPerson(new Person("Jan"));
-        main.insertContacts(jan, new Contact()._setAddress("Jan address 1"));
+        main.insertPersonContacts(jan, new Contact()._setAddress("Jan address 1"));
         Person janik = main.insertPerson(new Person("Janik"));
-        main.insertContacts(janik, new Contact()._setAddress("Janik address 1"));
+        main.insertPersonContacts(janik, new Contact()._setAddress("Janik address 1"));
         Person honza = main.insertPerson(new Person("Honza"));
-        main.insertContacts(honza, new Contact()._setAddress("Honza address 1"),
+        main.insertPersonContacts(honza, new Contact()._setAddress("Honza address 1"),
                 new Contact()._setAddress("Honza address 2"));
         Person honzik = main.insertPerson(new Person("Honzik"));
         Person andrej = main.insertPerson(new Person("Andrej"));
-        main.insertContacts(andrej, new Contact()._setAddress("Andrej address 1"));
+        main.insertPersonContacts(andrej, new Contact()._setAddress("Andrej address 1"));
 
-        // Book book1 = main.insertBook(new Book("The Adventures of Robin Hood", "978-0140367003"));
-        // Book book2 = main.insertBook(new Book("The Three Musketeers", "978-1897093634"));
-        // Movie movie1 = main.insertMovie(new Movie("Pippi Långstrump i Söderhavet", "abc", 82));
-        // Movie movie2 = main.insertMovie(new Movie("Die Another Day", "def", 95));
-        //
-        // main.createPersonLibrary(jan, book1, movie1);
-        // main.createPersonLibrary(honza, book2, movie2);
-        // main.createPersonLibrary(andrej, book1, book2, movie2);
-        //
-        // Library lib = main.insertLibrary(new Library("Alexandria Library"));
-        // Subscriber arnost = main.insertSubscriber(new Subscriber(lib, "Arnost"));
-        // Subscriber maria = main.insertSubscriber(new Subscriber(lib, "Maria"));
-        //
-        // main.insertBankAccount(new BankAccount(arnost, "BA")._setBaAccount("account 1"));
-        // main.insertBankAccount(new BankAccount(maria, "BA")._setBaAccount("account 2"));
-        // main.insertCreditCard(new CreditCard(arnost, "CC")._setCcNumber(123L));
-        // main.insertCreditCard(new CreditCard(maria, "CC")._setCcNumber(456L));
-        //
+        Library lib = main.insertLibrary(new Library("Alexandria Library"));
+        Subscriber janikS = main.insertLibrarySubscriber(lib,
+                new Subscriber(lib, "Janik Subscr")._setContact(jan.getContacts().get(0)));
+        Subscriber honzaS = main.insertLibrarySubscriber(lib,
+                new Subscriber(lib, "Honza Subscr")._setContact(honza.getContacts().get(0)));
+
+        main.insertBankAccount(new BankAccount(janikS, "BA")._setBaAccount("account 1"));
+        main.insertBankAccount(new BankAccount(honzaS, "BA")._setBaAccount("account 2"));
+        main.insertCreditCard(new CreditCard(janikS, "CC")._setCcNumber(123L));
+        main.insertCreditCard(new CreditCard(honzaS, "CC")._setCcNumber(456L));
+
+        Book book1 = main.insertBook(new Book("The Adventures of Robin Hood", "978-0140367003"));
+        Book book2 = main.insertBook(new Book("The Three Musketeers", "978-1897093634"));
+        Movie movie1 = main.insertMovie(new Movie("Pippi Långstrump i Söderhavet", "abc", 82));
+        Movie movie2 = main.insertMovie(new Movie("Die Another Day", "def", 95));
+
+        main.insertPersonLibrary(jan, book1, movie1);
+        main.insertPersonLibrary(honza, book2, movie2);
+        main.insertPersonLibrary(andrej, book1, book2, movie2);
+
         // // queries
         // list = main.listAll();
         // Assert.assertEquals(5, list.size());
