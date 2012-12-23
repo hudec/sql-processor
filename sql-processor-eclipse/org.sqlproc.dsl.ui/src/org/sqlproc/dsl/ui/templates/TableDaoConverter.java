5319cd3189a567a5f845b30c99c24fa838db45a4
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2012-12-23 10:28:43 +0100

    Issue #95: daogen

--- sql-processor-eclipse/org.sqlproc.dsl.ui/src/org/sqlproc/dsl/ui/templates/TableDaoConverter.java
+++ sql-processor-eclipse/org.sqlproc.dsl.ui/src/org/sqlproc/dsl/ui/templates/TableDaoConverter.java
@@ -3,7 +3,11 @@ package org.sqlproc.dsl.ui.templates;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.xtext.common.types.JvmType;
 import org.eclipse.xtext.scoping.IScopeProvider;
 import org.sqlproc.dsl.processorDsl.Artifacts;
 import org.sqlproc.dsl.property.ModelProperty;
@@ -12,12 +16,38 @@ public class TableDaoConverter extends TableMetaConverter {
 
     private boolean debug = false;
 
+    protected Set<String> daoIgnoreTables = new HashSet<String>();
+    protected Set<String> daoOnlyTables = new HashSet<String>();
+    protected Map<String, Set<String>> daoImplementsInterfaces;
+    protected JvmType daoControlClass;
+
     public TableDaoConverter() {
         super();
     }
 
     public TableDaoConverter(ModelProperty modelProperty, Artifacts artifacts, IScopeProvider scopeProvider) {
         super(modelProperty, artifacts, null);
+
+        Set<String> daoIgnoreTables = modelProperty.getDaoIgnoreTables(artifacts);
+        if (daoIgnoreTables != null) {
+            this.daoIgnoreTables.addAll(daoIgnoreTables);
+        }
+        Set<String> daoOnlyTables = modelProperty.getDaoOnlyTables(artifacts);
+        if (daoOnlyTables != null) {
+            this.daoOnlyTables.addAll(daoOnlyTables);
+        }
+        Map<String, Set<String>> daoImplementsInterfaces = modelProperty.getDaoImplementsInterfaces(artifacts);
+        if (daoImplementsInterfaces != null) {
+            this.daoImplementsInterfaces.putAll(daoImplementsInterfaces);
+        }
+        this.daoControlClass = modelProperty.getDaoControlClass(artifacts);
+
+        if (debug) {
+            System.out.println("daoIgnoreTables " + this.daoIgnoreTables);
+            System.out.println("daoOnlyTables " + this.daoOnlyTables);
+            System.out.println("daoImplementsInterfaces " + this.daoImplementsInterfaces);
+            System.out.println("daoControlClass " + this.daoControlClass);
+        }
     }
 
     public String getDaoDefinitions() {
