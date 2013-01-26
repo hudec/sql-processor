d0d4e699e074931b9277096b7da4fd79661290fb
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2013-01-26 19:09:51 +0100

    Issue #116: New grammar

--- sql-processor-eclipse/org.sqlproc.dsl/src/org/sqlproc/dsl/util/Constants.java
+++ sql-processor-eclipse/org.sqlproc.dsl/src/org/sqlproc/dsl/util/Constants.java
@@ -10,5 +10,5 @@ public class Constants {
     public static final String CONSTANT_USAGE = "const";
     public static final String MAPPING_USAGE_EXTENDED = "outx";
     public static final String MAPPING_USAGE = "out";
-    public static final String TABLE_USAGE = "col";
+    public static final String TABLE_USAGE = "tab";
 }
