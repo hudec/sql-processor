595091dfa6d29c2fa162212834a4dd15d673fb8b
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2013-05-20 07:16:35 +0200

    Issue #157: Ext JS Sample

--- simple-samples/simple-gui/app/app.js
+++ simple-samples/simple-gui/app/app.js
@@ -6,6 +6,10 @@ Ext.require('Ext.direct.*', function() {
 	Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
 });
 
+//Ext.onReady(function() {
+//        Ext.getBody().addCls(Ext.baseCSSPrefix + 'theme-gray');
+//});
+
 Ext.application({
 	name : 'SimpleWeb',
 	appFolder : "app",
