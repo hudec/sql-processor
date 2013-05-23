c3f7fc4e45e737476373cc847c61527b8f76b666
Tomas Hudec <hudec.tomas1@gmail.com>
2013-05-23 13:30:07 +0200

    Issue #157: Ext JS Sample

--- simple-samples/simple-gui/app/store/People.js
+++ simple-samples/simple-gui/app/store/People.js
@@ -4,12 +4,13 @@ Ext.define('SimpleWeb.store.People', {
 	alias : 'widget.personstore',
 	autoLoad : true,
 	remoteSort : true,
+	remoteFilter : true,
 	pageSize : 30,
 	autoSync : true,
 	sorters : [ {
 		property : 'lastName',
 		direction : 'ASC'
-	} ],
+	} ]
 	/*listeners : {
 		load : function(store) {
 			store.each(function(record) {
