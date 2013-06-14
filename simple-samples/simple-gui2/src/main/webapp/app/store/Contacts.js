d6b6317bb418ef99014a47c62b4990001c19449f
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2013-06-14 06:52:38 +0200

    Issue #159: Ext JS sample improvement

--- simple-samples/simple-gui2/src/main/webapp/app/store/Contacts.js
+++
@@ -1,8 +0,0 @@
-Ext.define('SimpleWeb.store.Contacts', {
-    extend : 'Ext.data.Store',
-    model : 'SimpleWeb.model.Contact',
-    autoLoad : false,
-    remoteSort : false,
-    pageSize : 30,
-    autoSync : true,
-});
