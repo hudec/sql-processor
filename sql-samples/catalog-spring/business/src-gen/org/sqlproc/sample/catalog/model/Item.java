ac225611c773d1b9a3e8e7f0a850e69e42c5fee3
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2013-10-20 11:36:36 +0200

    Issue #169: Allign simple-hsqldb with SQLEP 1.7.4

--- sql-samples/catalog-spring/business/src-gen/org/sqlproc/sample/catalog/model/Item.java
+++ sql-samples/catalog-spring/business/src-gen/org/sqlproc/sample/catalog/model/Item.java
@@ -20,135 +20,135 @@ public class Item implements Serializable {
   }
   
   private Long itemid;
-    
+  
   public Long getItemid() {
     return itemid;
   }
-    
+  
   public void setItemid(Long itemid) {
     this.itemid = itemid;
   }
-    
+  
   public Item _setItemid(Long itemid) {
     this.itemid = itemid;
     return this;
   }
   
   private String productid;
-    
+  
   public String getProductid() {
     return productid;
   }
-    
+  
   public void setProductid(String productid) {
     this.productid = productid;
   }
-    
+  
   public Item _setProductid(String productid) {
     this.productid = productid;
     return this;
   }
   
   private String name;
-    
+  
   public String getName() {
     return name;
   }
-    
+  
   public void setName(String name) {
     this.name = name;
   }
-    
+  
   public Item _setName(String name) {
     this.name = name;
     return this;
   }
   
   private String description;
-    
+  
   public String getDescription() {
     return description;
   }
-    
+  
   public void setDescription(String description) {
     this.description = description;
   }
-    
+  
   public Item _setDescription(String description) {
     this.description = description;
     return this;
   }
   
   private String imageurl;
-    
+  
   public String getImageurl() {
     return imageurl;
   }
-    
+  
   public void setImageurl(String imageurl) {
     this.imageurl = imageurl;
   }
-    
+  
   public Item _setImageurl(String imageurl) {
     this.imageurl = imageurl;
     return this;
   }
   
   private String imagethumburl;
-    
+  
   public String getImagethumburl() {
     return imagethumburl;
   }
-    
+  
   public void setImagethumburl(String imagethumburl) {
     this.imagethumburl = imagethumburl;
   }
-    
+  
   public Item _setImagethumburl(String imagethumburl) {
     this.imagethumburl = imagethumburl;
     return this;
   }
   
   private byte[] image;
-    
+  
   public byte[] getImage() {
     return image;
   }
-    
+  
   public void setImage(byte[] image) {
     this.image = image;
   }
-    
+  
   public Item _setImage(byte[] image) {
     this.image = image;
     return this;
   }
   
   private byte[] imagethumb;
-    
+  
   public byte[] getImagethumb() {
     return imagethumb;
   }
-    
+  
   public void setImagethumb(byte[] imagethumb) {
     this.imagethumb = imagethumb;
   }
-    
+  
   public Item _setImagethumb(byte[] imagethumb) {
     this.imagethumb = imagethumb;
     return this;
   }
   
   private BigDecimal price;
-    
+  
   public BigDecimal getPrice() {
     return price;
   }
-    
+  
   public void setPrice(BigDecimal price) {
     this.price = price;
   }
-    
+  
   public Item _setPrice(BigDecimal price) {
     this.price = price;
     return this;
@@ -163,7 +163,7 @@ public class Item implements Serializable {
     if (getClass() != obj.getClass())
       return false;
     Item other = (Item) obj;
-    if (!itemid.equals(other.itemid))
+    if (itemid == null || !itemid.equals(other.itemid))
       return false;
     return true;
   }  
@@ -172,7 +172,7 @@ public class Item implements Serializable {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + itemid.hashCode();
+    result = prime * result + ((itemid != null) ? itemid.hashCode() : 0);
     return result;
   }  
   
