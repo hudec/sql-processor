793e54448c25692e80ac440b4e7b1c2432844d9b
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-02-21 19:32:21 +0100

    Issue #207: Grammar 2.1.0

--- simple-samples/simple-mysql/src-gen/org/sample/model/ContactCtype.java
+++ simple-samples/simple-mysql/src-gen/org/sample/model/ContactCtype.java
@@ -1,42 +1,43 @@
 package org.sample.model;
 
-import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
 
-public enum ContactCtype implements Serializable {
-
-	HOME("0"), BUSINESS("1"), ABROAD("2");
-		
-	private static final long serialVersionUID = 1L;
-	
-	private static Map<String, ContactCtype> identifierMap = new HashMap<String, ContactCtype>();
-
-	static {
-		for (ContactCtype value : ContactCtype.values()) {
-			identifierMap.put(value.getValue(), value);
-		}
-	}
-
-	private String value;
-
-	private ContactCtype(String value) {
-		this.value = value;
-	}
-
-	public static ContactCtype fromValue(String value) {
-		ContactCtype result = identifierMap.get(value);
-		if (result == null) {
-			throw new IllegalArgumentException("No ContactCtype for value: " + value);
-		}
-		return result;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public String getName() {
-		return name();
-	}
+@SuppressWarnings("all")
+public enum ContactCtype {
+  HOME("0"),
+  
+  BUSINESS("1"),
+  
+  ABROAD("2");
+  private static Map<String, ContactCtype> identifierMap =  identifierMapBuild();
+  
+  public static Map<String, ContactCtype> identifierMapBuild() {
+    Map<String, ContactCtype> _identifierMap = new java.util.HashMap<String, ContactCtype>();
+    for (ContactCtype value : ContactCtype.values()) {
+    	_identifierMap.put(value.getValue(), value);
+    }
+    return _identifierMap;
+  }
+  
+  private String value;
+  
+  private ContactCtype(final String value) {
+    this.value = value;
+  }
+  
+  public static ContactCtype fromValue(final String value) {
+    ContactCtype result = identifierMap.get(value);
+    if (result == null) {
+    	throw new IllegalArgumentException("No ContactCtype for value: " + value);
+    }
+    return result;
+  }
+  
+  public String getValue() {
+    return value;
+  }
+  
+  public String getName() {
+    return name();
+  }
 }
