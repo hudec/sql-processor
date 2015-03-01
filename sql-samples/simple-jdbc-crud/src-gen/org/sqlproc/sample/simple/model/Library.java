e0dfb39fa327fd0cbba631d89d23aea357c5cbd7
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-03-01 15:24:46 +0100

    Issue #207: Grammar 2.1.0

--- sql-samples/simple-jdbc-crud/src-gen/org/sqlproc/sample/simple/model/Library.java
+++ sql-samples/simple-jdbc-crud/src-gen/org/sqlproc/sample/simple/model/Library.java
@@ -1,186 +1,178 @@
 package org.sqlproc.sample.simple.model;
+	
+import java.util.List;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
-import org.sqlproc.engine.annotation.Pojo;
-import org.sqlproc.sample.simple.model.PhysicalMedia;
-import org.sqlproc.sample.simple.model.Subscriber;
+import java.util.HashSet;
+import java.lang.reflect.InvocationTargetException;
+import org.apache.commons.beanutils.MethodUtils;
 
-@Pojo
-@SuppressWarnings("all")
 public class Library implements Serializable {
-  private final static long serialVersionUID = 1L;
-  
-  public final static int ORDER_BY_ID = 1;
-  
-  public Library() {
-  }
-  
-  public Library(final String name) {
-    setName(name);
-  }
-  
-  private Long id;
-  
-  public Long getId() {
-    return this.id;
-  }
-  
-  public void setId(final Long id) {
-    this.id = id;
-  }
-  
-  public Library _setId(final Long id) {
-    this.id = id;
-    return this;
-  }
-  
-  private String name;
-  
-  public String getName() {
-    return this.name;
-  }
-  
-  public void setName(final String name) {
-    this.name = name;
-  }
-  
-  public Library _setName(final String name) {
-    this.name = name;
-    return this;
-  }
-  
-  private List<Subscriber> subscribers = new java.util.ArrayList<Subscriber>();
-  
-  public List<Subscriber> getSubscribers() {
-    return this.subscribers;
-  }
-  
-  public void setSubscribers(final List<Subscriber> subscribers) {
-    this.subscribers = subscribers;
-  }
-  
-  public Library _setSubscribers(final List<Subscriber> subscribers) {
-    this.subscribers = subscribers;
-    return this;
-  }
-  
-  private List<PhysicalMedia> catalog = new java.util.ArrayList<PhysicalMedia>();
-  
-  public List<PhysicalMedia> getCatalog() {
-    return this.catalog;
-  }
-  
-  public void setCatalog(final List<PhysicalMedia> catalog) {
-    this.catalog = catalog;
-  }
-  
-  public Library _setCatalog(final List<PhysicalMedia> catalog) {
-    this.catalog = catalog;
-    return this;
-  }
-  
-  @Override
-  public boolean equals(final Object obj) {
-    if (this == obj)
-    	return true;
-    if (obj == null)
-    	return false;
-    if (getClass() != obj.getClass())
-    	return false;
-    Library other = (Library) obj;
-    if (id == null || !id.equals(other.id))
-    	return false;
-    return true;
-  }
-  
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id != null) ? id.hashCode() : 0);
-    return result;
-  }
-  
-  @Override
-  public String toString() {
-    return "Library [id=" + id + ", name=" + name + "]";
-  }
-  
-  public String toStringFull() {
-    return "Library [id=" + id + ", name=" + name + ", subscribers=" + subscribers + ", catalog=" + catalog + "]";
-  }
-  
-  public enum Association {
-    subscribers,
-    
-    catalog;
-  }
-  
-  private Set<String> initAssociations =  new java.util.HashSet<String>();
-  
-  public void setInit(final Library.Association... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (Association association : associations)
-    	initAssociations.add(association.name());
-  }
-  
-  public Library _setInit(final Library.Association... associations) {
-    setInit(associations);
-    return this;
-  }
-  
-  public void clearInit(final Library.Association... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (Association association : associations)
-    	initAssociations.remove(association.name());
-  }
-  
-  public Library _clearInit(final Library.Association... associations) {
-    clearInit(associations);
-    return this;
-  }
-  
-  public void setInit(final String... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (String association : associations)
-    	initAssociations.add(association);
-  }
-  
-  public Library _setInit(final String... associations) {
-    setInit(associations);
-    return this;
-  }
-  
-  public void clearInit(final String... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (String association : associations)
-    	initAssociations.remove(association);
-  }
-  
-  public Library _clearInit(final String... associations) {
-    clearInit(associations);
-    return this;
-  }
-  
-  public Boolean toInit(final Library.Association association) {
-    if (association == null)
-    	throw new IllegalArgumentException();
-    return initAssociations.contains(association.name());
-  }
-  
-  public Boolean toInit(final String association) {
-    if (association == null)
-    	throw new IllegalArgumentException();
-    return initAssociations.contains(association);
-  }
-  
-  public void clearAllInit() {
-    initAssociations = new java.util.HashSet<String>();
-  }
+	
+	private static final long serialVersionUID = 1L;
+	public static final int ORDER_BY_ID = 1;
+	
+	public Library() {
+	}
+		
+	public Library(String name) {
+		this.name = name;
+	}
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Library _setId(Long id) {
+		this.id = id;
+		return this;
+	}
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Library _setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	private List<Subscriber> subscribers = new ArrayList<Subscriber>();
+
+	public List<Subscriber> getSubscribers() {
+		return subscribers;
+	}
+
+	public void setSubscribers(List<Subscriber> subscribers) {
+		this.subscribers = subscribers;
+	}
+
+	public Library _setSubscribers(List<Subscriber> subscribers) {
+		this.subscribers = subscribers;
+		return this;
+	}
+
+	private List<PhysicalMedia> catalog = new ArrayList<PhysicalMedia>();
+
+	public List<PhysicalMedia> getCatalog() {
+		return catalog;
+	}
+
+	public void setCatalog(List<PhysicalMedia> catalog) {
+		this.catalog = catalog;
+	}
+
+	public Library _setCatalog(List<PhysicalMedia> catalog) {
+		this.catalog = catalog;
+		return this;
+	}
+
+	public enum Association {
+		subscribers, catalog
+	}
+
+	private Set<String> initAssociations = new HashSet<String>();
+
+	public void setInit(Association... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (Association association : associations)
+			initAssociations.add(association.name());
+	}
+	
+	public Library	_setInit(Association... associations) {
+		setInit(associations);
+		return this;
+	}
+
+	public void clearInit(Association... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (Association association : associations)
+			initAssociations.remove(association.name());
+	}
+
+	public Library _clearInit(Association... associations) {
+		clearInit(associations);
+		return this;
+	}
+
+	public void setInit(String... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (String association : associations)
+			initAssociations.add(association);
+	}
+
+	public Library _setInit(String... associations) {
+		setInit(associations);
+		return this;
+	}
+
+	public void clearInit(String... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (String association : associations)
+			initAssociations.remove(association);
+	}
+
+	public Library _clearInit(String... associations) {
+		clearInit(associations);
+		return this;
+	}
+
+	public Boolean toInit(String association) {
+		if (association == null)
+			throw new IllegalArgumentException();
+		return initAssociations.contains(association);
+	}
+
+	public void clearAllInit() {
+		initAssociations = new HashSet<String>();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Library other = (Library) obj;
+		if (id == null || !id.equals(other.id))
+		return false;
+		return true;
+	}	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id != null) ? id.hashCode() : 0);
+		return result;
+	}	
+
+	@Override
+	public String toString() {
+		return "Library [id=" + id + ", name=" + name + "]";
+	}
+
+	public String toStringFull() {
+		return "Library [id=" + id + ", name=" + name + ", subscribers=" + subscribers + ", catalog=" + catalog + "]";
+	}
 }
--- sql-samples/simple-jdbc-crud/src-gen/org/sqlproc/sample/simple/model/Media.java
+++ sql-samples/simple-jdbc-crud/src-gen/org/sqlproc/sample/simple/model/Media.java
@@ -2,264 +2,256 @@ package org.sqlproc.sample.simple.model;
 
 import java.io.Serializable;
 import java.util.Set;
-import org.sqlproc.engine.annotation.Pojo;
-import org.sqlproc.sample.simple.model.Performer;
+import java.util.HashSet;
+import java.lang.reflect.InvocationTargetException;
+import org.apache.commons.beanutils.MethodUtils;
 
-@Pojo
-@SuppressWarnings("all")
 public class Media implements Serializable {
-  private final static long serialVersionUID = 1L;
-  
-  public final static int ORDER_BY_ID = 1;
-  
-  public final static int ORDER_BY_AUTHOR = 2;
-  
-  public Media() {
-  }
-  
-  public Media(final String title) {
-    setTitle(title);
-  }
-  
-  private Long id;
-  
-  public Long getId() {
-    return this.id;
-  }
-  
-  public void setId(final Long id) {
-    this.id = id;
-  }
-  
-  public Media _setId(final Long id) {
-    this.id = id;
-    return this;
-  }
-  
-  private String title;
-  
-  public String getTitle() {
-    return this.title;
-  }
-  
-  public void setTitle(final String title) {
-    this.title = title;
-  }
-  
-  public Media _setTitle(final String title) {
-    this.title = title;
-    return this;
-  }
-  
-  private Performer author;
-  
-  public Performer getAuthor() {
-    return this.author;
-  }
-  
-  public void setAuthor(final Performer author) {
-    this.author = author;
-  }
-  
-  public Media _setAuthor(final Performer author) {
-    this.author = author;
-    return this;
-  }
-  
-  @Override
-  public boolean equals(final Object obj) {
-    if (this == obj)
-    	return true;
-    if (obj == null)
-    	return false;
-    if (getClass() != obj.getClass())
-    	return false;
-    Media other = (Media) obj;
-    if (id == null || !id.equals(other.id))
-    	return false;
-    return true;
-  }
-  
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id != null) ? id.hashCode() : 0);
-    return result;
-  }
-  
-  @Override
-  public String toString() {
-    return "Media [id=" + id + ", title=" + title + "]";
-  }
-  
-  public String toStringFull() {
-    return "Media [id=" + id + ", title=" + title + ", author=" + author + "]";
-  }
-  
-  public enum Attribute {
-    author;
-  }
-  
-  private Set<String> nullValues =  new java.util.HashSet<String>();
-  
-  public void setNull(final Media.Attribute... attributes) {
-    if (attributes == null)
-    	throw new IllegalArgumentException();
-    for (Attribute attribute : attributes)
-    	nullValues.add(attribute.name());
-  }
-  
-  public Media _setNull(final Media.Attribute... attributes) {
-    setNull(attributes);
-    return this;
-  }
-  
-  public void clearNull(final Media.Attribute... attributes) {
-    if (attributes == null)
-    	throw new IllegalArgumentException();
-    for (Attribute attribute : attributes)
-    	nullValues.remove(attribute.name());
-  }
-  
-  public Media _clearNull(final Media.Attribute... attributes) {
-    clearNull(attributes);
-    return this;
-  }
-  
-  public void setNull(final String... attributes) {
-    if (attributes == null)
-    	throw new IllegalArgumentException();
-    for (String attribute : attributes)
-    	nullValues.add(attribute);
-  }
-  
-  public Media _setNull(final String... attributes) {
-    setNull(attributes);
-    return this;
-  }
-  
-  public void clearNull(final String... attributes) {
-    if (attributes == null)
-    	throw new IllegalArgumentException();
-    for (String attribute : attributes)
-    	nullValues.remove(attribute);
-  }
-  
-  public Media _clearNull(final String... attributes) {
-    clearNull(attributes);
-    return this;
-  }
-  
-  public Boolean isNull(final Media.Attribute attribute) {
-    if (attribute == null)
-    	throw new IllegalArgumentException();
-    return nullValues.contains(attribute.name());
-  }
-  
-  public Boolean isNull(final String attrName) {
-    if (attrName == null)
-    	throw new IllegalArgumentException();
-    return nullValues.contains(attrName);
-  }
-  
-  public Boolean isDef(final String attrName) {
-    if (attrName == null)
-    	throw new IllegalArgumentException();
-    if (nullValues.contains(attrName))
-    	return true;
-    try {
-    	Object result = org.apache.commons.beanutils.MethodUtils.invokeMethod(this, "get" + attrName.substring(0, 1).toUpperCase() + attrName.substring(1, attrName.length()), null);
-    	return (result != null) ? true : false;
-    } catch (NoSuchMethodException e) {
-    } catch (IllegalAccessException e) {
-    	throw new RuntimeException(e);
-    } catch (java.lang.reflect.InvocationTargetException e) {
-    	throw new RuntimeException(e);
-    }
-    try {
-    	Object result = org.apache.commons.beanutils.MethodUtils.invokeMethod(this, "is" + attrName.substring(0, 1).toUpperCase() + attrName.substring(1, attrName.length()), null);
-    	return (result != null) ? true : false;
-    } catch (NoSuchMethodException e) {
-    } catch (IllegalAccessException e) {
-    	throw new RuntimeException(e);
-    } catch (java.lang.reflect.InvocationTargetException e) {
-    	throw new RuntimeException(e);
-    }
-    return false;
-  }
-  
-  public void clearAllNull() {
-    nullValues = new java.util.HashSet<String>();
-  }
-  
-  public enum Association {
-    author;
-  }
-  
-  private Set<String> initAssociations =  new java.util.HashSet<String>();
-  
-  public void setInit(final Media.Association... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (Association association : associations)
-    	initAssociations.add(association.name());
-  }
-  
-  public Media _setInit(final Media.Association... associations) {
-    setInit(associations);
-    return this;
-  }
-  
-  public void clearInit(final Media.Association... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (Association association : associations)
-    	initAssociations.remove(association.name());
-  }
-  
-  public Media _clearInit(final Media.Association... associations) {
-    clearInit(associations);
-    return this;
-  }
-  
-  public void setInit(final String... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (String association : associations)
-    	initAssociations.add(association);
-  }
-  
-  public Media _setInit(final String... associations) {
-    setInit(associations);
-    return this;
-  }
-  
-  public void clearInit(final String... associations) {
-    if (associations == null)
-    	throw new IllegalArgumentException();
-    for (String association : associations)
-    	initAssociations.remove(association);
-  }
-  
-  public Media _clearInit(final String... associations) {
-    clearInit(associations);
-    return this;
-  }
-  
-  public Boolean toInit(final Media.Association association) {
-    if (association == null)
-    	throw new IllegalArgumentException();
-    return initAssociations.contains(association.name());
-  }
-  
-  public Boolean toInit(final String association) {
-    if (association == null)
-    	throw new IllegalArgumentException();
-    return initAssociations.contains(association);
-  }
-  
-  public void clearAllInit() {
-    initAssociations = new java.util.HashSet<String>();
-  }
+	
+	private static final long serialVersionUID = 1L;
+	public static final int ORDER_BY_ID = 1;
+	public static final int ORDER_BY_AUTHOR = 2;
+	
+	public Media() {
+	}
+		
+	public Media(String title) {
+		this.title = title;
+	}
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Media _setId(Long id) {
+		this.id = id;
+		return this;
+	}
+
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Media _setTitle(String title) {
+		this.title = title;
+		return this;
+	}
+
+	private Performer author;
+
+	public Performer getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Performer author) {
+		this.author = author;
+	}
+
+	public Media _setAuthor(Performer author) {
+		this.author = author;
+		return this;
+	}
+
+	public enum Association {
+		author
+	}
+
+	private Set<String> initAssociations = new HashSet<String>();
+
+	public void setInit(Association... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (Association association : associations)
+			initAssociations.add(association.name());
+	}
+	
+	public Media	_setInit(Association... associations) {
+		setInit(associations);
+		return this;
+	}
+
+	public void clearInit(Association... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (Association association : associations)
+			initAssociations.remove(association.name());
+	}
+
+	public Media _clearInit(Association... associations) {
+		clearInit(associations);
+		return this;
+	}
+
+	public void setInit(String... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (String association : associations)
+			initAssociations.add(association);
+	}
+
+	public Media _setInit(String... associations) {
+		setInit(associations);
+		return this;
+	}
+
+	public void clearInit(String... associations) {
+		if (associations == null)
+			throw new IllegalArgumentException();
+		for (String association : associations)
+			initAssociations.remove(association);
+	}
+
+	public Media _clearInit(String... associations) {
+		clearInit(associations);
+		return this;
+	}
+
+	public Boolean toInit(String association) {
+		if (association == null)
+			throw new IllegalArgumentException();
+		return initAssociations.contains(association);
+	}
+
+	public void clearAllInit() {
+		initAssociations = new HashSet<String>();
+	}
+
+	public enum Attribute {
+		author
+	}
+
+	private Set<String> nullValues = new HashSet<String>();
+
+	public void setNull(Attribute... attributes) {
+		if (attributes == null)
+			throw new IllegalArgumentException();
+		for (Attribute attribute : attributes)
+			nullValues.add(attribute.name());
+	}
+
+	public Media _setNull(Attribute... attributes) {
+		setNull(attributes);
+		return this;
+	}
+
+	public void clearNull(Attribute... attributes) {
+		if (attributes == null)
+			throw new IllegalArgumentException();
+		for (Attribute attribute : attributes)
+			nullValues.remove(attribute.name());
+	}
+
+	public Media _clearNull(Attribute... attributes) {
+		clearNull(attributes);
+		return this;
+	}
+
+	public void setNull(String... attributes) {
+		if (attributes == null)
+			throw new IllegalArgumentException();
+		for (String attribute : attributes)
+			nullValues.add(attribute);
+	}
+
+	public Media _setNull(String... attributes) {
+		setNull(attributes);
+		return this;
+	}
+
+	public void clearNull(String... attributes) {
+		if (attributes == null)
+			throw new IllegalArgumentException();
+		for (String attribute : attributes)
+			nullValues.remove(attribute);
+	}
+
+	public Media _clearNull(String... attributes) {
+		clearNull(attributes);
+		return this;
+	}
+
+	public Boolean isNull(String attrName) {
+		if (attrName == null)
+			throw new IllegalArgumentException();
+		return nullValues.contains(attrName);
+	}
+
+	public Boolean isNull(Attribute attribute) {
+		if (attribute == null)
+			throw new IllegalArgumentException();
+		return nullValues.contains(attribute.name());
+	}
+
+	public Boolean isDef(String attrName) {
+		if (attrName == null)
+			throw new IllegalArgumentException();
+		if (nullValues.contains(attrName))
+			return true;
+		try {
+			Object result = MethodUtils.invokeMethod(this, "get" + attrName.substring(0, 1).toUpperCase() + attrName.substring(1, attrName.length()), null);
+			return (result != null) ? true : false;
+		} catch (NoSuchMethodException e) {
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+		try {
+			Object result = MethodUtils.invokeMethod(this, "is" + attrName.substring(0, 1).toUpperCase() + attrName.substring(1, attrName.length()), null);
+			return (result != null) ? true : false;
+		} catch (NoSuchMethodException e) {
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+		return false;
+	}
+
+	public void clearAllNull() {
+		nullValues = new HashSet<String>();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Media other = (Media) obj;
+		if (id == null || !id.equals(other.id))
+		return false;
+		return true;
+	}	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id != null) ? id.hashCode() : 0);
+		return result;
+	}	
+
+	@Override
+	public String toString() {
+		return "Media [id=" + id + ", title=" + title + "]";
+	}
+
+	public String toStringFull() {
+		return "Media [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
 }
