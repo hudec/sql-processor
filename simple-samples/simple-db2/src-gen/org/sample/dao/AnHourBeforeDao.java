eb3b125461c31c5228484fbbf10d44adae24c0d4
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-03-29 10:07:36 +0200

    Issue #207: Grammar 2.1.0

--- simple-samples/simple-db2/src-gen/org/sample/dao/AnHourBeforeDao.java
+++ simple-samples/simple-db2/src-gen/org/sample/dao/AnHourBeforeDao.java
@@ -1,60 +1,61 @@
 package org.sample.dao;
-	
+
 import java.sql.Timestamp;
+
 import org.sample.model.AnHourBefore;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.sqlproc.engine.SqlControl;
-import org.sqlproc.engine.SqlCrudEngine;
 import org.sqlproc.engine.SqlEngineFactory;
 import org.sqlproc.engine.SqlQueryEngine;
-import org.sqlproc.engine.SqlProcedureEngine;
 import org.sqlproc.engine.SqlSession;
 import org.sqlproc.engine.SqlSessionFactory;
-import org.sqlproc.engine.impl.SqlStandardControl;
-import org.sample.model.AnHourBefore;
 
+@SuppressWarnings("all")
 public class AnHourBeforeDao {
-	protected final Logger logger = LoggerFactory.getLogger(getClass());
+    protected final Logger logger = org.slf4j.LoggerFactory.getLogger(getClass());
 
-	protected SqlEngineFactory sqlEngineFactory;
-	protected SqlSessionFactory sqlSessionFactory;
-			
-	public AnHourBeforeDao() {
-	}
-			
-	public AnHourBeforeDao(SqlEngineFactory sqlEngineFactory) {
-		this.sqlEngineFactory = sqlEngineFactory;
-	}
-			
-	public AnHourBeforeDao(SqlEngineFactory sqlEngineFactory, SqlSessionFactory sqlSessionFactory) {
-		this.sqlEngineFactory = sqlEngineFactory;
-		this.sqlSessionFactory = sqlSessionFactory;
-	}
-	
-	public Timestamp anHourBefore(SqlSession sqlSession, AnHourBefore anHourBefore, SqlControl sqlControl) {
-		if (logger.isTraceEnabled()) {
-			logger.trace("anHourBefore: " + anHourBefore + " " + sqlControl);
-		}
-		SqlQueryEngine sqlFunAnHourBeforeDao = sqlEngineFactory.getCheckedQueryEngine("FUN_AN_HOUR_BEFORE");
-		java.util.List<AnHourBefore> list = sqlFunAnHourBeforeDao.query(sqlSession, AnHourBefore.class, anHourBefore, sqlControl);
-		if (logger.isTraceEnabled()) {
-			logger.trace("anHourBefore result: " + list);
-		}
-		return (list != null && !list.isEmpty()) ? list.get(0).getResult() : null;
-	}
-	public Timestamp anHourBefore(AnHourBefore anHourBefore, SqlControl sqlControl) {
-		return anHourBefore(sqlSessionFactory.getSqlSession(), anHourBefore, sqlControl);
-	}
-	public Timestamp anHourBefore(SqlSession sqlSession, AnHourBefore anHourBefore) {
-		return anHourBefore(sqlSession, anHourBefore, null);
-	}
-	public Timestamp anHourBefore(AnHourBefore anHourBefore) {
-		return anHourBefore(anHourBefore, null);
-	}
+    public AnHourBeforeDao() {
+    }
+
+    public AnHourBeforeDao(final SqlEngineFactory sqlEngineFactory) {
+        this.sqlEngineFactory = sqlEngineFactory;
+    }
+
+    public AnHourBeforeDao(final SqlEngineFactory sqlEngineFactory, final SqlSessionFactory sqlSessionFactory) {
+        this.sqlEngineFactory = sqlEngineFactory;
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    protected SqlEngineFactory sqlEngineFactory;
+
+    protected SqlSessionFactory sqlSessionFactory;
+
+    public Timestamp anHourBefore(final SqlSession sqlSession, final AnHourBefore AnHourBefore, SqlControl sqlControl) {
+        if (logger.isTraceEnabled()) {
+            logger.trace("sql anHourBefore: " + AnHourBefore + " " + sqlControl);
+        }
+        SqlQueryEngine sqlFunAnHourBeforeDao = sqlEngineFactory.getCheckedQueryEngine("FUN_AN_HOUR_BEFORE");
+        java.util.List<AnHourBefore> list = sqlFunAnHourBeforeDao.query(sqlSession, AnHourBefore.class, AnHourBefore,
+                sqlControl);
+        // org.sqlproc.engine.SqlProcedureEngine sqlProcAnHourBeforeDao =
+        // sqlEngineFactory.getCheckedProcedureEngine("FUN_AN_HOUR_BEFORE");
+        // java.util.List<AnHourBefore> list = sqlFunAnHourBeforeDao.query(sqlSession, AnHourBefore.class, AnHourBefore,
+        // sqlControl);
+        if (logger.isTraceEnabled()) {
+            logger.trace("sql anHourBefore result: " + list);
+        }
+        return (list != null && !list.isEmpty()) ? list.get(0).getResult() : null;
+    }
+
+    public Timestamp anHourBefore(final AnHourBefore AnHourBefore, SqlControl sqlControl) {
+        return anHourBefore(sqlSessionFactory.getSqlSession(), AnHourBefore, sqlControl);
+    }
+
+    public Timestamp anHourBefore(final SqlSession sqlSession, final AnHourBefore AnHourBefore) {
+        return anHourBefore(sqlSession, AnHourBefore, null);
+    }
+
+    public Timestamp anHourBefore(final AnHourBefore AnHourBefore) {
+        return anHourBefore(AnHourBefore, null);
+    }
 }
--- simple-samples/simple-db2/src-gen/org/sample/model/AnHourBefore.java
+++ simple-samples/simple-db2/src-gen/org/sample/model/AnHourBefore.java
@@ -1,52 +1,50 @@
 package org.sample.model;
-	
-import java.sql.Timestamp;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import org.sqlproc.engine.annotation.Pojo;
 
+@Pojo
+@SuppressWarnings("all")
 public class AnHourBefore implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	public AnHourBefore() {
-	}
-
-	private Timestamp t;
-
-	public Timestamp getT() {
-		return t;
-	}
-
-	public void setT(Timestamp t) {
-		this.t = t;
-	}
-
-	public AnHourBefore _setT(Timestamp t) {
-		this.t = t;
-		return this;
-	}
-
-	private Timestamp result;
-
-	public Timestamp getResult() {
-		return result;
-	}
-
-	public void setResult(Timestamp result) {
-		this.result = result;
-	}
-
-	public AnHourBefore _setResult(Timestamp result) {
-		this.result = result;
-		return this;
-	}
-
-	@Override
-	public String toString() {
-		return "AnHourBefore [result=" + result + ", t=" + t + "]";
-	}
-
-	public String toStringFull() {
-		return "AnHourBefore [t=" + t + ", result=" + result + "]";
-	}
+  private final static long serialVersionUID = 1L;
+  
+  private Timestamp t;
+  
+  public Timestamp getT() {
+    return this.t;
+  }
+  
+  public void setT(final Timestamp t) {
+    this.t = t;
+  }
+  
+  public AnHourBefore _setT(final Timestamp t) {
+    this.t = t;
+    return this;
+  }
+  
+  private Timestamp result;
+  
+  public Timestamp getResult() {
+    return this.result;
+  }
+  
+  public void setResult(final Timestamp result) {
+    this.result = result;
+  }
+  
+  public AnHourBefore _setResult(final Timestamp result) {
+    this.result = result;
+    return this;
+  }
+  
+  @Override
+  public String toString() {
+    return "AnHourBefore [result=" + result + ", t=" + t + "]";
+  }
+  
+  public String toStringFull() {
+    return "AnHourBefore [t=" + t + ", result=" + result + "]";
+  }
 }
