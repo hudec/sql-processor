aa1dc14768f0d7ce1d6fffaeb5613259e6aac8e7
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-04-05 18:39:02 +0200

    Issue #207: Grammar 2.1.0

---
+++ sql-samples/simple-spring-pojo/src-gen/org/sqlproc/sample/simple/form/FormSimpleFunction.java
@@ -0,0 +1,42 @@
+package org.sqlproc.sample.simple.form;
+
+import java.sql.Timestamp;
+import org.sqlproc.engine.annotation.Pojo;
+
+@Pojo
+@SuppressWarnings("all")
+public class FormSimpleFunction {
+  private Timestamp time;
+  
+  public Timestamp getTime() {
+    return this.time;
+  }
+  
+  public void setTime(final Timestamp time) {
+    this.time = time;
+  }
+  
+  public FormSimpleFunction _setTime(final Timestamp time) {
+    this.time = time;
+    return this;
+  }
+  
+  private Timestamp time2;
+  
+  public Timestamp getTime2() {
+    return this.time2;
+  }
+  
+  public void setTime2(final Timestamp time2) {
+    this.time2 = time2;
+  }
+  
+  public FormSimpleFunction _setTime2(final Timestamp time2) {
+    this.time2 = time2;
+    return this;
+  }
+  
+  public String toStringFull() {
+    return "FormSimpleFunction [time=" + time + ", time2=" + time2 + "]";
+  }
+}
