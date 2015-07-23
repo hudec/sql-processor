7694a37eb6fb9facf1c3b8eece79b6f41d7617d0
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2015-07-23 08:17:48 +0200

    Issue #214: Port to Java7

--- sql-samples/test.sh
+++ sql-samples/test.sh
@@ -1,41 +1,41 @@
 # !/bin/bash
 
-mvn6 clean install
+mvn clean install
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd simple-hibernate/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd ../simple-jdbc/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd ../simple-jdbc-crud/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd ../simple-jdbc-dao/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd ../simple-jdbc-pojo/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd ../simple-spring/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 cd ../simple-spring-pojo/
-mvn6 exec:java
+mvn exec:java
 if [ $? -ne 0 ]; then
 	exit 1
 fi
