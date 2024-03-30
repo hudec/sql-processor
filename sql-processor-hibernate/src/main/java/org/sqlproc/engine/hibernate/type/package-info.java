cdb9f71c0e33e4b06550e8c9962b4e5d670df8a2
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2024-03-30 19:10:47 +0100

    JDK21

--- sql-processor-hibernate/src/main/java/org/sqlproc/engine/hibernate/type/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * All the META types staff for the Hibernate stack devoted to special handling of input/output values.
- * The most important class is the {@link org.sqlproc.engine.hibernate.type.HibernateTypeFactory}.
- * 
- * @author <a href="mailto:Vladimir.Hudec@gmail.com">Vladimir Hudec</a>
- */
-package org.sqlproc.engine.hibernate.type;
